9acb6677d064822d216873e71702e83c33882e85
Jo <johj703@nate.com>
2024-08-12 12:09:48 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [user, setUser] = useState({ name: '홍길동', age: 25 });
-  const [items, setItems] = useState(['사과', '바나나', '오렌지']);
+const App = () => {
+  return <div>App</div>;
+};
 
-  // TODO: 구조분해할당(Destructuring) 코드를 여기 아래에 작성하세요.
-  const { name, age } = user;
-  const [firstItem, secondItem, thirdItem] = items;
-
-  return (
-    <div>
-      <h1>사용자 정보</h1>
-      <p>이름: {name}</p>
-      <p>나이: {age}</p>
-      <h2>아이템 목록</h2>
-      <ul>
-        <li>{firstItem}</li>
-        <li>{secondItem}</li>
-        <li>{thirdItem}</li>
-      </ul>
-      <button
-        onClick={() => 
-          setUser({name: "김철수", age: 30})
-        }
-      >
-        변경하기 함수
-      </button>
-    </div>
-  );
-}
-
-export default App
+export default App;
