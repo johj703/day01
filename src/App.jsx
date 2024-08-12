06fa93cdce8a7b01e9a8983b03e252cf5f7ddd7c
Jo <johj703@nate.com>
2024-08-12 12:21:01 +0900

    함수 이름과 alert 내용 변경

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ const App = () => {
 
   const { name, age } = user;
 
-  const handleSubmit = () => {
-    alert("이벤트");
+  const addNewItem = () => {
+    alert("새로운 아이템 추가하는 함수가 호출됐어요!");
   };
 
   return (
@@ -20,7 +20,7 @@ const App = () => {
       <p>나이: {age}</p>
       <h2>사용자의 아이템 목록</h2>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={addNewItem}>
         <input />
         <button>추가</button>
       </form>
