833d792e7873c115016f5ee4703d3a83fd104048
Jo <johj703@nate.com>
2024-08-12 12:12:34 +0900

    useState로 상태 선언

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [user, setUser] = useState({
+    name: "홍길동",
+    age: 25,
+  });
+  const [items, setItems] = useState(["사과", "바나나", "오렌지"]);
+
   return <div>App</div>;
 };
 
